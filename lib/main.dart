732d6369b6c521646880f8ae07165c442db61711
Trenggana Natadirja <trenggana.natadirja@klb.co.id>
2019-08-20 17:35:56 +0700

    onChange show input text

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var routes = <String, WidgetBuilder> {
-      MyItemsPage.routeName: (BuildContext context) => new MyItemsPage(title: "My Items Page"),
-    };
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: new MyHomePage(title: "Flutter Demo Home Page"),
-      routes: routes,
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    home: new MyTextInput()
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class MyTextInput extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  MyTextInputState createState() => new MyTextInputState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  void _incrementCounter() {
-    Navigator.pushNamed(context, MyItemsPage.routeName);
-  }
+class MyTextInputState extends State<MyTextInput> {
+  String result = '';
 
   @override
   Widget build(BuildContext context) {
-    var button = new IconButton(icon: new Icon(Icons.access_alarm), onPressed: _onButtonPressed);
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Text('Dog'),
-          new Text('Cat'),
-          button
-        ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _onButtonPressed() {
-    Navigator.pushNamed(context, MyItemsPage.routeName);
-  }
-}
-
-class MyItemsPage extends StatefulWidget {
-  MyItemsPage({Key key, this.title}) : super(key: key);
-
-  static const String routeName = "/MyItemsPage";
-
-  final String title;
-
-  @override
-  _MyItemsPageState createState() => new _MyItemsPageState();
-}
-
-class _MyItemsPageState extends State<MyItemsPage> {
-  @override
-  Widget build(BuildContext context) {
-    var button = new IconButton(icon: new Icon(Icons.arrow_back), onPressed: _onButtonPressed);
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
+      appBar: new AppBar(title: new Text("Input Text"), backgroundColor: Colors.deepOrange),
       body: new Container(
-        child: new Column(
-          children: <Widget>[
-            new Text('Item1'),
-            new Text('Item2'),
-            button
-          ],
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new TextField(
+                decoration: new InputDecoration(
+                  hintText: "Type in here"
+                ),
+                onChanged: (String str) {
+                  setState(() {
+                    result = str;
+                  });
+                }
+              ),
+              new Text(result)
+            ],
+          ),
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _onFloatingActionButtonPressed,
-        tooltip: 'Add',
-        child: new Icon(Icons.add),
-      ),
     );
-  }
-
-  void _onFloatingActionButtonPressed() {
-  }
-
-  void _onButtonPressed() {
-    Navigator.pop(context);
   }
 }
