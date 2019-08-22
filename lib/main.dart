d3c1f6fcd757dfdae5b368b15473b5cb62021018
Trenggana Natadirja <trenggana.natadirja@klb.co.id>
2019-08-22 10:26:35 +0700

    Simple drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,9 @@
-import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+import './home_page.dart';
 
 void main() {
   runApp(new MaterialApp(
     home: new HomePage()
   ));
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  HomePageState createState() => new HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-
-  List data;
-
-  Future<String> getData() async {
-    var response = await http.get(
-      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
-      headers: {
-        "Accept": "application/json"
-      }
-    );
-
-    this.setState(() {
-      data = jsonDecode(response.body);
-    });
-    
-    print(data[1]["title"]);
-    
-    return "Success!";
-  }
-
-  @protected
-  @mustCallSuper
-  void initState(){
-    super.initState();
-    this.getData();
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(title: new Text("List Views"), backgroundColor: Colors.blue),
-      body: new ListView.builder(
-        itemCount: data == null ? 0 : data.length,
-        itemBuilder: (BuildContext context, int index){
-          return new Card(
-            child: Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Text(data[index]["title"]),
-            )
-          );
-        }
-      ),
-    );
-  }
 }
